0c6babf11a4cd03e65182e9378390a42dabf26ac
Harvey Yang <harveyyang2002@gmail.com>
2016-02-28 14:35:57 -0500

    fixed team insertion

--- Team Insertion.sql
+++ Team Insertion.sql
@@ -1,15 +1,89 @@
-﻿INSERT INTO warehouse VALUES
-	(1, '612 Inverness Drive, Buffalo NY, 14215');
+INSERT INTO team VALUES
+	('Boston Celtics', '00001');
 
-INSERT INTO warehouse VALUES
-	(2, '455 Maiden Lane, Tampa FL, 33604');
+INSERT INTO team VALUES
+	('Brooklyn Nets', '00002');
 
-INSERT INTO warehouse VALUES
-	(3, '748 Bridge Street, Houston TX, 77016');
+INSERT INTO team VALUES
+	('New York Knicks', '00003');
 
-INSERT INTO warehouse VALUES
-	(4, '479 Grove Avenue, Seattle WA, 98144');
+INSERT INTO team VALUES
+	('Philadelphia 76ers', '00004');
 
-INSERT INTO warehouse VALUES
-	(5, '532 Franklin Street, San Jose CA, 95127');
+INSERT INTO team VALUES
+	('Toronto Raptors', '00005');
 
+INSERT INTO team VALUES
+	('Chicago Bulls', '00006'); -- babyyyy
+
+INSERT INTO team VALUES
+	('Cleveland Cavaliers', '00007');
+
+INSERT INTO team VALUES
+	('Detroit Pistons', '00008');
+
+INSERT INTO team VALUES
+	('Indiana Pacers', '00009');
+
+INSERT INTO team VALUES
+	('Milwaukee Bucks', '00010');
+
+INSERT INTO team VALUES
+	('Atlanta Hawks', '00011');
+
+INSERT INTO team VALUES
+	('Charlotte Hornets', '00012');
+
+INSERT INTO team VALUES
+	('Miami Heat', '00013');
+
+INSERT INTO team VALUES
+	('Orlando Magic', '00014');
+
+INSERT INTO team VALUES
+	('Washington Wizards', '00015');
+
+INSERT INTO team VALUES
+	('Dallas Mavericks', '00016');
+
+INSERT INTO team VALUES
+	('Houston Rockets', '00017');
+
+INSERT INTO team VALUES
+	('Memphis Grizzlies', '00018');
+
+INSERT INTO team VALUES
+	('New Orleans Pelicans', '00019');
+
+INSERT INTO team VALUES
+	('San Antonio Spurs', '00020');
+
+INSERT INTO team VALUES
+	('Denver Nuggets', '00021'); 
+
+INSERT INTO team VALUES
+	('Minnesota Timberwolves', '00022');
+
+INSERT INTO team VALUES
+	('Oklahoma City Thunder', '00023');
+
+INSERT INTO team VALUES
+	('Portland Trail Blazers', '00024');
+
+INSERT INTO team VALUES
+	('Utah Jazz', '00025');
+
+INSERT INTO team VALUES
+	('Golden State Warriors', '00026');
+
+INSERT INTO team VALUES
+	('Los Angeles Clippers', '00027');
+
+INSERT INTO team VALUES
+	('Los Angeles Lakers', '00028');
+
+INSERT INTO team VALUES
+	('Phoenix Suns', '00029');
+
+INSERT INTO team VALUES
+	('Sacramento Kings', '00030');
