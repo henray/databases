f098c3688199ff325110d230dba22f49569b6c94
Henry Lin <hl1721@gmail.com>
2016-02-27 22:53:20 -0500

    Fix ProductOrder and Warehouse .sql

--- Warehouse Insertion.sql
+++ Warehouse Insertion.sql
@@ -1,15 +1,5 @@
-﻿INSERT INTO warehouse VALUES
-	(1, '612 Inverness Drive, Buffalo NY, 14215');
-
-INSERT INTO warehouse VALUES
-	(2, '455 Maiden Lane, Tampa FL, 33604');
-
-INSERT INTO warehouse VALUES
-	(3, '748 Bridge Street, Houston TX, 77016');
-
-INSERT INTO warehouse VALUES
-	(4, '479 Grove Avenue, Seattle WA, 98144');
-
-INSERT INTO warehouse VALUES
-	(5, '532 Franklin Street, San Jose CA, 95127');
-
+﻿INSERT INTO Warehouse VALUES (1, '612 Inverness Drive, Buffalo NY, 14215');
+INSERT INTO Warehouse VALUES(2, '455 Maiden Lane, Tampa FL, 33604');
+INSERT INTO Warehouse VALUES(3, '748 Bridge Street, Houston TX, 77016');
+INSERT INTO Warehouse VALUES(4, '479 Grove Avenue, Seattle WA, 98144');
+INSERT INTO Warehouse VALUES(5, '532 Franklin Street, San Jose CA, 95127');
